6d341f482be1c8bf4c19d170d88e135451c11929
FasterThanIE <tomicawarr@gmail.com>
2025-07-30 19:25:43 +0200

    * Add missing request

---
+++ app/Http/Requests/StoreVideoRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreVideoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'video' => 'required|mimes:mp4,mov,avi,wmv|max:512000',
+        ];
+    }
+}
